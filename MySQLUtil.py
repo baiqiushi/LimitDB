41122a0ea74c7c1e35cb3ef50b4475fdfbcd43a4
baiqiushi <baiqiushi@gmail.com>
2018-07-23 11:26:08 -0700

    Test Subset

---
+++ MySQLUtil.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': '3979',
+  'host': 'localhost',
+  'database': 'limitdb',
+  'raise_on_warnings': True,
+  'use_pure': False,
+}
+
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
+
+
+def query(sql):
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
+def close():
+    cursor.close()
+    conn.close()
+
+
+def GetCoordinate(tableName, keyword, limit):
+    sql="select x, y from limitdb." + tableName + " where match(text) against (\'+" + keyword + "\' in boolean mode)"
+    if limit >= 0:
+        sql += " limit " + str(limit)
+    return query(sql)
---
+++ TestSubset.py
@@ -0,0 +1,56 @@
+import numpy as np
+import MySQLUtil as db
+# import time
+import math
+
+database = 'MySQL'
+tableName = 'coordtweets'
+keywords = ['job', 'water', 'soccer']
+
+
+# p_limit1 < p_limit2, check whether p_limit1 is subset of p_limit2
+def isSubsetFromDB(p_tableName, p_keyword, p_limit1, p_limit2):
+    # start = time.time()
+    ar1 = np.array(db.GetCoordinate(p_tableName, p_keyword, p_limit1))
+    ar2 = np.array(db.GetCoordinate(p_tableName, p_keyword, p_limit2))
+    # end = time.time()
+    # print 'DB query time: ', (end - start), ' s'
+    # start = time.time()
+    for c in ar1:
+        if c not in ar2:
+            return False
+    # end = time.time()
+    # print 'Computation time: ', (end - start), ' s'
+    return True
+
+
+def testSubsetForKeyword(p_tableName, p_keyword):
+    print 'Table: ' + p_tableName
+    print 'Keyword: ' + p_keyword
+
+    failed_cases = []
+    for k in [1, 10, 100, 1000, 10000, 50000, 100000, 500000, 1000000, 2000000]:
+        for delta_k in [1, math.ceil(0.1*k), math.ceil(0.3*k), math.ceil(0.5*k)]:
+            result = isSubsetFromDB(p_tableName, p_keyword, int(k), int(k + delta_k))
+            msg = 'Checking whether limit ' + str(int(k)) + ' is subset of limit ' + str(int(k + delta_k))
+            if result:
+                print msg + ' --- YES'
+            else:
+                failed_cases.append([int(k), int(k + delta_k)])
+
+    if len(failed_cases) > 0:
+        print '***********************************************'
+        print 'Failed limit k pairs:'
+        for failure in failed_cases:
+            print '[' + failure[0] + ', ' + failure[1] + ']'
+
+
+print '================================================='
+print '  ' + database + '  Experiments - 1 Test Subset'
+print '================================================='
+
+for keyword in keywords:
+    testSubsetForKeyword(tableName, keyword)
+    print '------------------------------------------------'
+
+db.close()
