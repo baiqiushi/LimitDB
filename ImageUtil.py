b712d408a02cbc31714750bf0ce5c8dd8074760b
baiqiushi <baiqiushi@gmail.com>
2018-08-14 15:25:17 -0700

    Add heatmap draw

--- ImageUtil.py
+++ ImageUtil.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
-import cv2 as cv  # pip install opencv-python
+# import cv2 as cv  # pip install opencv-python
+import Conf
+import DatabaseFactory
+import QualityUtil
+import numpy as np
 
 
-def DrawImage(matrix, fn):
+def DrawScatter(matrix, p_file):
     x = []
     y = []
     for r in matrix:
@@ -15,14 +19,28 @@ def DrawImage(matrix, fn):
     # plt.xticks([yt for yt in range(-170,-60,10)])
     plt.ylim(15, 70)
     plt.xlim(-170, -60)
-    plt.savefig(fn, bbox_inches='tight', dpi=350)
+    plt.savefig(p_file, bbox_inches='tight', dpi=350)
     plt.close()
 
 
-def GetGrayImage(p_file):
-    image = cv.imread(p_file)
-    image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-    return image
+def DrawHeat(matrix, p_normalFactor, p_file):
+    if p_normalFactor == 'sum':
+        l_sum = np.sum(matrix)
+        matrix = matrix / l_sum
+    elif p_normalFactor == 'max':
+        l_max = np.max(matrix)
+        matrix = matrix / l_max
+    r, c = np.nonzero(matrix)
+    print matrix[r, c]
+    plt.imshow(matrix, cmap='hot', interpolation='nearest')
+    plt.savefig(p_file)
+    plt.close()
+
+
+# def GetGrayImage(p_file):
+#     image = cv.imread(p_file)
+#     image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+#     return image
 
 
 def GetSimilarityFromImages(p1, p2):
@@ -39,9 +57,23 @@ def GetSimilarityFromImages(p1, p2):
             else:
                 b = 0
             if a == b == 1:
-                one +=1
+                one += 1
             else:
                 if a != b:
                     diff += 1
     similarity = 1-diff/((1344-55)*(1864-133)-one)
     return similarity
+
+
+def testDrawHeat(p_keyword):
+    db = DatabaseFactory.getDatabase(Conf.DATABASE)
+    totalCoordinates = np.array(db.GetCoordinate(Conf.TABLE, p_keyword, -1))
+    if len(totalCoordinates) == 0:
+        return
+    img = QualityUtil.coordinatesToImage(totalCoordinates)
+    DrawHeat(np.fliplr(img).transpose(), 'no', 'heat_' + p_keyword + '.png')
+    DrawHeat(np.fliplr(img).transpose(), 'sum', 'heat_' + p_keyword + '_sum.png')
+    DrawHeat(np.fliplr(img).transpose(), 'max', 'heat_' + p_keyword + '_max.png')
+
+
+testDrawHeat('job')
