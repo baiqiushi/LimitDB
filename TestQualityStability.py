92c79aed2614d77c693e35376238a4bb81337710
baiqiushi <baiqiushi@gmail.com>
2018-08-21 22:33:52 -0700

    Compare limit with random quality stability

--- TestQualityStability.py
+++ TestQualityStability.py
@@ -17,11 +17,12 @@ tableName = Conf.TABLE
 db = DatabaseFactory.getDatabase(database)
 
 # Choose 6 specific keywords
-keywords = [KeywordsUtil.pickAllInFrequencyRange(5418, 5419)[0],  # soccer
-            KeywordsUtil.pickAllInFrequencyRange(106875, 106877)[0],  # rain
-            KeywordsUtil.pickAllInFrequencyRange(211000, 212000)[0],  # love
-            KeywordsUtil.pickAllInFrequencyRange(500000, 600000)[0],  # appli
-            KeywordsUtil.pickAllInFrequencyRange(990000, 1000000)[0]]  # work
+# keywords = [keywordsutil.pickallinfrequencyrange(5418, 5419)[0],  # soccer
+#             keywordsutil.pickallinfrequencyrange(106875, 106877)[0],  # rain
+#             keywordsutil.pickallinfrequencyrange(211000, 212000)[0],  # love
+#             keywordsutil.pickallinfrequencyrange(500000, 600000)[0],  # appli
+#             keywordsutil.pickallinfrequencyrange(990000, 1000000)[0]]  # work
+keywords = [('job', 1000)]
 
 k_percentages = [30, 50, 70, 90]
 
@@ -35,6 +36,39 @@ def plotCurves(p_fileName, p_curves, p_title, p_showLegend=True):
         print i_label
         print p_curves[i_label]
         plt.plot(range(1, 21, 1), p_curves[i_label], label=(str(i_label) + '%'))
+    plt.xlabel('the (i)th run')
+    plt.ylabel('Quality(%)')
+    plt.title(p_title)
+    plt.grid(True)
+    if p_showLegend:
+        plt.legend()
+    plt.savefig(pp, format='pdf')
+    pp.close()
+
+
+# plot given p_curves1 all as one series in color blue and p_curves2 all as one series in color red into one image
+
+# p_curves1: in the format as {label0: [y0, y1, ..., y19], label1: [y0, y1, ..., y19], ...}
+# p_curves2: same as p_curves1
+def plot2SeriesCurves(p_fileName, p_label1, p_curves1, p_label2, p_curves2, p_title, p_showLegend=True):
+    pp = PdfPages(p_fileName + '.pdf')
+    plt.figure()
+    i1 = 0
+    for i_label in p_curves1.keys():
+        i1 += 1
+        # assign label only once
+        if i1 == 1:
+            plt.plot(range(1, 21, 1), p_curves1[i_label], 'bo-', label=p_label1)
+        else:
+            plt.plot(range(1, 21, 1), p_curves1[i_label], 'bo-')
+    i2 = 0
+    for i_label in p_curves2.keys():
+        i2 += 1
+        # assign label only once
+        if i2 == 1:
+            plt.plot(range(1, 21, 1), p_curves2[i_label], 'ro-', label=p_label2)
+        else:
+            plt.plot(range(1, 21, 1), p_curves2[i_label], 'ro-', label=p_label2)
     plt.xlabel('the (i)th run')
     plt.ylabel('Quality(%)')
     plt.title(p_title)
@@ -68,9 +102,11 @@ for keyword in keywords:
 
 # 2. Run same query for each keyword 20 times and calculate the quality
 # {'soccer': {30: [20 qualities], 50: [20 qualities], ...}, 'home': {...}}
-qualities = {}
+qualities_random = {}
+qualities_limit = {}
 for keyword in keywords:
-    qualities[keyword[0]] = {}
+    qualities_random[keyword[0]] = {}
+    qualities_limit[keyword[0]] = {}
 
 print 'Start running queries with random k percentages ...'
 progress = 0
@@ -79,11 +115,17 @@ for i in range(0, 20, 1):
     for k_p in k_percentages:
         for keyword in keywords:
             l_random_ratio = float(k_p) / 100.0
-            l_coordinates = db.GetCoordinateRandomly(tableName, keyword[0], l_random_ratio)
-            l_quality = QualityUtil.phQualityOfCoordinates(totalCoordinates[keyword[0]], l_coordinates)
-            if k_p not in qualities[keyword[0]]:
-                qualities[keyword[0]][k_p] = []
-            qualities[keyword[0]][k_p].append(l_quality * 100)
+            L_limit_k = int(float(k_p) * keyword[1] / 100.0)
+            l_coordinates_random = db.GetCoordinateRandomly(tableName, keyword[0], l_random_ratio)
+            l_coordinates_limit = db.GetCoordinate(tableName, keyword[0], L_limit_k)
+            l_quality_random = QualityUtil.phQualityOfCoordinates(totalCoordinates[keyword[0]], l_coordinates_random)
+            l_quality_limit = QualityUtil.phQualityOfCoordinates(totalCoordinates[keyword[0]], l_coordinates_limit)
+            if k_p not in qualities_random[keyword[0]]:
+                qualities_random[keyword[0]][k_p] = []
+            if k_p not in qualities_limit[keyword[0]]:
+                qualities_limit[keyword[0]][k_p] = []
+            qualities_random[keyword[0]][k_p].append(l_quality_random * 100)
+            qualities_limit[keyword[0]][k_p].append(l_quality_limit * 100)
             progress += 1
             print '[Total time]', time.time() - t0, \
                 '[Progress]', str(progress * 100 / (len(keywords) * len(k_percentages) * 20)) + '%'
@@ -92,7 +134,9 @@ for i in range(0, 20, 1):
 print 'Plotting images ...'
 for keyword in keywords:
     i_fileName = keyword[0] + '_' + str(keyword[1])
-    plotCurves(i_fileName, qualities[keyword[0]], 'Random Function Qualities Stability - Keyword "' + keyword[0] + '"')
+    plot2SeriesCurves(i_fileName, 'random', qualities_random[keyword[0]],
+                      'limit', qualities_limit[keyword[0]],
+                      'Qualities Stability - Keyword "' + keyword[0] + '"')
 
 end = time.time()
 print '================================================='
