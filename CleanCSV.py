3b3bcfe6387b23bbd36e2d748d4a8c34a9184d4d
baiqiushi <baiqiushi@gmail.com>
2018-09-18 01:26:51 -0700

    universal new line

--- CleanCSV.py
+++ CleanCSV.py
@@ -4,7 +4,7 @@ csvFileName = "test"
 
 with open(csvFileName + "_out.csv", 'w') as outcsvFile:
         csvWriter = csv.writer(outcsvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        with open(csvFileName + ".csv", 'rb') as incsvFile:
+        with open(csvFileName + ".csv", 'rbU') as incsvFile:
             csvReader = csv.reader(incsvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
             for row in csvReader:
                 if len(row) < 4 or len(row) > 4:
