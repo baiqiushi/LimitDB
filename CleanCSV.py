9c6e12a6d8a0c5fa385be0d9a6ea3d724488b296
baiqiushi <baiqiushi@gmail.com>
2018-09-18 12:39:20 -0700

    Add check id, x, y to be numbers

--- CleanCSV.py
+++ CleanCSV.py
@@ -8,8 +8,24 @@ with open(csvFileName + "_out.csv", 'w') as outcsvFile:
             csvReader = csv.reader(incsvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
             for row in csvReader:
                 if len(row) < 4 or len(row) > 4:
-                    print row
+                    print '# -', row
                     continue
                 else:
+                    if not row[0].isdigit():
+                        print 'id -', row, row[0]
+                        continue
+
+                    try:
+                        x = float(row[2])
+                    except ValueError:
+                        print 'x -', row, row[2]
+                        continue
+
+                    try:
+                        y = float(row[3])
+                    except ValueError:
+                        print 'y -', row, row[3]
+                        continue
+
                     csvWriter.writerow(row)
 
