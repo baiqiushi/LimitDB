707f2f66d90d814ce43a40815435a95c98c66736
Qiushi Bai <baiqiushi@gmail.com>
2018-10-11 12:48:52 -0400

    testAll.sh

--- testAll.sh
+++ testAll.sh
@@ -12,6 +12,11 @@
 #python -u generateCurves.py -b limitdb2 -t coord_tweets_sorted -f1 1000000 -f2 1250000 |& tee -a generateCurves.log
 #python -u generateCurves.py -b limitdb2 -t coord_tweets_sorted -f1 5000000 -f2 10000000 |& tee -a generateCurves.log
 
+python -u generateCurves.py -b limitdb2 -t coord_tweets_combined -f1 130000 -f2 135000 |& tee -a generateCurves.log
+python -u generateCurves.py -b limitdb2 -t coord_tweets_combined -f1 500000 -f2 550000 |& tee -a generateCurves.log
+python -u generateCurves.py -b limitdb2 -t coord_tweets_combined -f1 1000000 -f2 1250000 |& tee -a generateCurves.log
+python -u generateCurves.py -b limitdb2 -t coord_tweets_combined -f1 5000000 -f2 10000000 |& tee -a generateCurves.log
+
 ## 10M
 
 #python -u generateCurves.py -b limitdb -t coord_tweets -f1 130000 -f2 135000 |& tee -a generateCurves.log
@@ -26,8 +31,8 @@
 
 ## 100M
 
-python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 130000 -f2 135000 |& tee -a generateCurves.log
-python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 500000 -f2 550000 |& tee -a generateCurves.log
-python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 1000000 -f2 1250000 |& tee -a generateCurves.log
-python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 5000000 -f2 10000000 |& tee -a generateCurves.log
+# python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 130000 -f2 135000 |& tee -a generateCurves.log
+# python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 500000 -f2 550000 |& tee -a generateCurves.log
+# python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 1000000 -f2 1250000 |& tee -a generateCurves.log
+# python -u generateCurves.py -b limitdb1 -t coord_tweets -f1 5000000 -f2 10000000 |& tee -a generateCurves.log
 
