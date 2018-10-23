a05a1b2da6c6dfe3606a71e8d1d76d75cb81259a
baiqiushi <baiqiushi@gmail.com>
2018-10-22 20:39:43 -0700

    refined KValuesGenerator

--- KValuesGenerator.py
+++ KValuesGenerator.py
@@ -6,7 +6,8 @@ import DatabaseFactory
 import QualityUtil
 
 # Common config
-database = Conf.DBTYPE
+dbtype = Conf.DBTYPE
+database = Conf.DATABASE
 tableName = Conf.TABLE
 
 # Config for this experiment
@@ -18,20 +19,20 @@ x_scale = 1
 y_scale = 1
 
 # Keywords frequency range
-min_freq = 5000
-max_freq = 4000000
+min_freq = 100000
+max_freq = 500000
 
 # target qualities
-qualities = range(70, 100, 5)
+qualities = [85]
 
 
 def generateKValues(p_min_freq, p_max_freq):
-    db = DatabaseFactory.getDatabase(database)
+    db = DatabaseFactory.getDatabase(dbtype, database)
 
     l_min_freq = int(p_min_freq)
     l_max_freq = int(p_max_freq)
 
-    keywords = KeywordsUtil.pickAllInFrequencyRange(l_min_freq, l_max_freq)
+    keywords = KeywordsUtil.pickInFrequencyRange(db, l_min_freq, l_max_freq)
 
     t = time.time()
     with open(csvFileName_k_values, 'w') as csvFile_k_values:
@@ -45,18 +46,15 @@ def generateKValues(p_min_freq, p_max_freq):
                 progress += 1
                 k_values = []
                 k_ratios = []
-                k_values.extend(keyword)
-                k_ratios.extend(keyword)
+                k_values.append(keyword)
+                k_ratios.append(keyword)
 
                 # 1. Get total coordinates of this keyword
                 t0 = time.time()
-                totalCoordinates = db.GetCoordinate(tableName, keyword[0], -1)
+                totalCoordinates = db.GetCoordinate(tableName, keyword, -1)
                 if len(totalCoordinates) == 0:
                     skipped += 1
                     continue
-                if len(totalCoordinates) != keyword[1]:
-                    print '[' + keyword[0] + ']', \
-                        'frequency = ' + str(keyword[1]) + ', len(perfect) = ' + str(len(totalCoordinates))
 
                 # 2. for each target quality calculate the k value using binary search
                 t1 = time.time()
@@ -73,8 +71,7 @@ def generateKValues(p_min_freq, p_max_freq):
                 csvWriter_k_ratios.writerow(k_ratios)
                 t3 = time.time()
                 # 4. output time information to console
-                print keyword[0], \
-                    ", [freq]", keyword[1], \
+                print keyword, \
                     ", [query]", t1 - t0, \
                     ", [k_values/ratios]", t2 - t1, \
                     ", [write]", t3 - t2, \
@@ -86,7 +83,7 @@ def generateKValues(p_min_freq, p_max_freq):
 
 
 print '================================================='
-print '  ' + database + '  Experiments - 4.3 K Values Generating'
+print '  ' + dbtype + '  Experiments - 4.3 K Values Generating'
 print '================================================='
 print 'table:', tableName
 print 'frequency range:[', min_freq, ',', max_freq, ']'
@@ -97,7 +94,7 @@ start = time.time()
 count = generateKValues(min_freq, max_freq)
 end = time.time()
 print '================================================='
-print '  ' + database + '  Results - 4.2 K Values Generating'
+print '  ' + dbtype + '  Results - 4.2 K Values Generating'
 print '================================================='
 print 'table:', tableName
 print 'frequency range:[', min_freq, ',', max_freq, ']'
